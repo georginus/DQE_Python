82ab06e35a8be605992d9c08a86adf924284a994
George Chichenkov <georginus@gmail.com>
2021-09-15 13:22:32 +0300

    1

--- Task01.py
+++ Task01.py
@@ -1,5 +1,14 @@
-i = 1
-n = 100
-for i in range(0, n):
-    print(i)
+from random import randrange
+
+# 100 random int values generating
+i = 0 # start loop from i
+n = 100 # end loop of n
+r = 1000 # boarding of random values
+rnd = []
+for i in range(i, n):
+    rnd.append(randrange(r))
     i+1
+
+
+print(rnd)
+#
