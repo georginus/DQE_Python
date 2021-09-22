112ae9ac8fca276745d893a60222726e0b1806a7
George Chichenkov <georginus@gmail.com>
2021-09-22 18:30:25 +0300

    1

---
+++ Task02.py
@@ -0,0 +1,19 @@
+# 1. create a list of random number of dicts (from 2 to 10)
+## dict's random numbers of keys should be letter,
+## dict's values should be a number (0-100),
+### example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+from random import randrange
+import random
+import string
+
+
+def func(n):
+    mydict = {}
+    for i in range(n):
+        mydict[random.choice(string.ascii_lowercase)] = randrange(100)
+    return mydict
+
+
+dict = func(26)
+print(len(dict))
+print(dict)
