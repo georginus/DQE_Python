508c8d8ed92d1d81657749a5c24f3c8e331c9969
George Chichenkov <georginus@gmail.com>
2021-09-29 17:43:13 +0300

    1

--- Task03.py
+++ Task03.py
@@ -1,63 +1,22 @@
 import re
 
-qqq = f"homEwork:
+text = r"""homEwork:
+	tHis iz your homeWork, copy these Text to variable.
 
-tHis
-iz
-your
-homeWork, copy
-these
-Text
-to
-variable.
+	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
 
-You
-NEED
-TO
-normalize
-it
-fROM
-letter
-CASEs
-point
-oF
-View.also, create
-one
-MORE
-senTENCE
-witH
-LAST
-WoRDS
-of
-each
-existING
-SENtence and add
-it
-to
-the
-END
-OF
-this
-Paragraph.
+	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
 
-it
-iZ
-misspeLLing
-here.fix“iZ” with correct “ is ”, but ONLY when it Iz a mistAKE.
+	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 
-last
-iz
-TO
-calculate
-nuMber
-OF
-Whitespace
-characteRS in this
-Tex.caREFULL, not only
-Spaces, but
-ALL
-whitespaces.I
-got
-87.")
+# print(text)
 
-print(text)
+new_text = text.lower().capitalize()# .replace(r'\n+', ' ', re.I) # all text in lowercase and
+new_text = re.sub(r'\n+', '\n', new_text).strip() # removing white spaces and replace several \n to only one \n
+new_text = re.sub(r'\t+', ' ', new_text).strip()
+# new_text = re.sub(r'\s+', ' ', new_text)
+new_text = '. '.join(map(lambda s: s.strip().capitalize(), new_text.split('.')))
+
+
+
+print(new_text)
