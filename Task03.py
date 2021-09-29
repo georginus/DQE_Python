d5056291b7325ce41a7913becc4442ef8ac833f4
George Chichenkov <georginus@gmail.com>
2021-09-29 21:03:44 +0300

    1

--- Task03.py
+++ Task03.py
@@ -1,6 +1,6 @@
 import re
 
-text = r"""homEwork:
+base_text = r"""homEwork:
 	tHis iz your homeWork, copy these Text to variable.
 
 	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
@@ -11,12 +11,27 @@ text = r"""homEwork:
 
 # print(text)
 
-new_text = text.lower().capitalize()# .replace(r'\n+', ' ', re.I) # all text in lowercase and
-new_text = re.sub(r'\n+', '\n', new_text).strip() # removing white spaces and replace several \n to only one \n
-new_text = re.sub(r'\t+', ' ', new_text).strip()
-# new_text = re.sub(r'\s+', ' ', new_text)
-new_text = '. '.join(map(lambda s: s.strip().capitalize(), new_text.split('.')))
+lower_text = base_text.lower() # all text in lowercase and
+extra_tabs_text = re.sub(r'\t+', ' ', lower_text).strip() # removing extra tabs
+extra_spaces__text = re.sub(r'\s+', ' ', lower_text) # removing extra spaces
+sentences_text = '. '.join(map(lambda s: s.strip().capitalize(), extra_spaces__text.split('.'))) # beautiful sentences
+iz_replace_text = sentences_text.replace(r"“iz”", r" “iz”").replace(" iz", ' is') # replace iz to is # replace 'Fix“iz”'
+new_text = iz_replace_text
 
 
+# Function which returns last word
+def word_list(string):
+    # split by space and converting
+    # string to list and
+    lis = list(string.split(" "))
+    print (lis)
+    # length of list
+    length = len(lis)
+
+    # returning last element in list
+    return lis[length - 1]
+
+qlist = word_list(new_text)
+qlist.find('\.')
 
 print(new_text)
