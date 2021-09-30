002fabd70f16655ed72a3b670c03f1fba5e95698
George Chichenkov <georginus@gmail.com>
2021-10-01 00:34:15 +0300

    1

--- Task03.py
+++ Task03.py
@@ -25,25 +25,18 @@ def word_list(string):
     return lis
 
 
+# Function convert list to string
 def listToString(s):
-    # initialize an empty string
     str1 = " "
-
-    # return string
     return (str1.join(s))
 
 
-qlist = word_list(text_formatted)  # text in list by words
-# print(qlist)
+list_by_words = word_list(text_formatted)  # text in list by words
 
-# for i in range(len(qlist)-1):
-#    j = qlist[i].replace(r'.', '')
-#    print(j)
-
-
-for i in qlist:
+# last words sentence generating
+for i in list_by_words:
     r = re.compile(r'\b(\w+[.])')
-    last_words_list = list(filter(r.match, qlist))
+    last_words_list = list(filter(r.match, list_by_words))
     last_sentence = [last_words_list[w].replace(r'.', '') for w in range(len(last_words_list))]
 
 last_sentence = listToString(last_sentence)
@@ -53,13 +46,26 @@ for i in range(len(text_formatted)):
     paragraph_list = list(text_formatted.split("\n\t"))
 
 # format Paragraphs and fill temp_text string
-temp_text = ''
+final_text = ''
 for sentence in range(len(paragraph_list)):
     sentence_formatted = '. '.join(map(lambda s: s.strip().capitalize(), paragraph_list[sentence].split('.')))
     sentence1_formatted = sentence_formatted
     if sentence1_formatted.endswith('add it to the end of this paragraph. '):
-        temp_text += sentence1_formatted + last_sentence.capitalize()+'. '+ '\n\t'
+        final_text += sentence1_formatted + last_sentence.capitalize() + '. ' + '\n\t'
     else:
-        temp_text += sentence1_formatted + '\n\t'
+        final_text += sentence1_formatted + '\n\t'
 
-print(temp_text)
+print(final_text)
+
+
+def check_space(string):
+    count = 0
+    # loop for search each index
+    for i in string:
+        if i == re.sub(r'\b(\s+)', '  ', final_text):
+            count += 1
+
+    return count
+
+
+print("number of spaces:", check_space(final_text))
