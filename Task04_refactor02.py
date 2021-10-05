a04abcf76162aa3f7358492004f4f669361f0022
George Chichenkov <georginus@gmail.com>
2021-10-05 13:05:15 +0300

    1

--- Task04_refactor02.py
+++ Task04_refactor02.py
@@ -2,6 +2,7 @@
 ## dict's random numbers of keys should be letter,
 ## dict's values should be a number (0-100),
 ### example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+import inspect
 from random import randrange
 import random
 import string
@@ -13,41 +14,42 @@ temp_dict = {}  # create a temporary dict for keys existence check
 common_dict = {}  # create a common dict for final result
 
 
-def max_dict_elements(char_num=26):  # number of lowercase English letters
-    max_dict_elem = int(char_num)
-    return max_dict_elem
+def params(max_dict_elements=26, max_dict_value=100, random_dict_num=10):
+    params_dict = {}
+    params_dict.update(max_dict_elements=max_dict_elements, max_dict_value=max_dict_value,
+                       random_dict_num=random_dict_num)
+    return params_dict
 
 
-def max_dict_value(max_val=100):  # max dict random value
-    max_dict_val = int(max_val)
-    return max_dict_val
+params_dict = params()
 
 
-def random_dict_num(dict_num):  # random dict numbers
-    rnd_dict_num = int(dict_num)
-    return rnd_dict_num
-
-
-def define_dict(max_dict_elements):  # create function for 1 random dict creation
-    dict_elem = max_dict_elements
-    max_dict_val = max_dict_value()
+def define_dict(max_dict_elements, max_dict_value):  # create function for 1 random dict creation
     random_dict = {}  # create a random dict
-    for i in range(dict_elem):  # define number of random elements
+    for i in range(max_dict_elements):  # define number of random elements
         random_dict[random.choice(string.ascii_lowercase)] = randrange(
-            max_dict_val)  # create a pair of random letter and random value from 0 to 100
-    return random_dict  # return random dict generated
+            max_dict_value)  # create a pair of random letter and random value from 0 to 100
+    return random_dict
 
 
-def dict_count(random_dict_num=10):
-    cnt = random_dict_num
-    dict_elem = max_dict_elements()
-    for dict_count in range(cnt):  # generating random numbers of random dicts from 2 to 10
-        list_dict.append(define_dict(dict_elem))  # call define_dict function and insert new element in the list_dict
-        # list after each run
+filtered_params_dict = {k: v for (k, v) in params_dict.items() if
+                        k in inspect.getfullargspec(define_dict)[0]}
+
+random_dict = define_dict(**filtered_params_dict)
+
+
+def list_dicts(max_dict_elements, random_dict_num):
+    for dict_count in range(10):  # generating random numbers of random dicts from 2 to 10
+        list_dict.append(define_dict(
+            max_dict_elements))  # call define_dict function and insert new element in the list_dict list after each run
     return list_dict
 
 
-list_dict = dict_count()
+filtered_params_list_dict = {k: v for (k, v) in params_dict.items() if
+                             k in inspect.getfullargspec(list_dicts)[0]}
+list_dict = list_dicts(**filtered_params_list_dict)
+print(list_dicts)
+
 for i in range(len(list_dict)):  # run loop for all elements (dicts) in the list_dict list
     for key, value in list_dict[i].items():  # for each pair in the current element (dict) in the list_dict list do
         if key in temp_dict.keys():  # if key exists in the temp_dict
@@ -55,7 +57,7 @@ for i in range(len(list_dict)):  # run loop for all elements (dicts) in the list
         max_value = value  # define var for max value
         dict_num = i  # define var for dict number
         num_appearance = 1  # define var for number of key appearance
-        for j in range(i + 1, len(list_dict)):  # loop to sfind keys in others dicts
+        for j in range(i + 1, len(list_dict)):  # loop to find keys in others dicts
             try:  # if the key found in the comparison dict do
                 if list_dict[j].get(key) > max_value:  # if comparison value > max_value
                     max_value = list_dict[j].get(key)  # then update max_value with comparison value
