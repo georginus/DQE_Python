3bc3246c088de06a7f4e13faa355b73f00aa9d1f
George Chichenkov <georginus@gmail.com>
2021-10-07 15:27:10 +0300

    1

--- Task05/Task05.py
+++ Task05/Task05.py
@@ -13,4 +13,22 @@ human = MyExample('Human', 2)
 spider.printIt()
 human.printIt()
 
+class Post:
+    def __init__(self, name):
+        self.name = name
+
+class Adv(Post):
+    def customer(self):
+        print('customer is Epam')
+    pass
+
+post = Post('News')
+adv = Adv('Adv')
+
+print(post.name)
+print(adv.name,' ',adv.customer())
+
+
+
+
 
