a7074a2af65b488c75137a2ff6cd098f87482abb
George Chichenkov <georginus@gmail.com>
2021-10-09 20:26:30 +0300

    1

--- Task05/Task05_classes.py
+++ Task05/Task05_classes.py
@@ -6,6 +6,7 @@ class Post:
         self.name = name
         self.text = text
         self.post_date = datetime.today()
+        self.date_formatted = self.post_date.strftime('%d/%m/%Y %H.%M')
 
     def printName(self):
         print(self.name)
@@ -19,22 +20,40 @@ class PrivateAd(Post):
         Post.__init__(self, name, text)
         self.end_date = datetime.strptime(end_date, '%d/%m/%Y').date()
         self.delta = (self.end_date - self.post_date.date()).days
+        self.end_date_formatted = self.end_date.strftime('%d/%m/%Y')
 
-    def printAdv(self):
-        print(f'{self.name} ------------------\n{self.text}\nActual until: {self.end_date}, {self.delta} days left\n------------------------------')
+    def printPost(self):
+        print(f'{self.name} -------------------\n{self.text}\nActual until: {self.end_date_formatted}, {self.delta} days '
+              f'left\n------------------------------\n')
 
 
 class News(Post):
-    def __init__(self, name, text, end_date):
+    def __init__(self, name, text, city):
         Post.__init__(self, name, text)
-        self.end_date = datetime.strptime(end_date, '%d/%m/%Y').date()
-        self.delta = (self.end_date - self.post_date.date()).days
+        self.city = city
 
-    def printAdv(self):
-        print(f'{self.name} ------------------\n{self.text}\nActual until: {self.end_date}, {self.delta} days left\n------------------------------')
+    def printPost(self):
+        print(f'{self.name} -------------------------\n{self.text}\n{self.city}, {self.date_formatted}\n'
+              f'------------------------------\n')
 
 
-post = Post('News', datetime.today())
+class LifeHack(Post):
+    def __init__(self, name, text, hashtag):
+        Post.__init__(self, name, text)
+        self.hashtag = hashtag
 
-qqq = PrivateAd('Private Ad', 'TEXT', '31/10/2021')
-qqq.printAdv()
+    def printPost(self):
+        print(f'{self.name} -------------------------\n{self.text}\n#{self.hashtag}, {self.date_formatted}\n'
+              f'------------------------------\n')
+
+
+ad1 = PrivateAd('Private Ad', 'TEXT', '31/10/2021')
+ad1.printPost()
+
+news1 = News('News', 'News text', 'Minsk')
+news1.printPost()
+
+lifehack1 = LifeHack('LifeHack', 'Text', 'Food')
+lifehack1.printPost()
+
+de
