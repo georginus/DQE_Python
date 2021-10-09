0da60ac4450dad656e000d05a61a2ba2cbe2408b
George Chichenkov <georginus@gmail.com>
2021-10-09 19:36:02 +0300

    1

--- Task05/Task05_classes.py
+++ Task05/Task05_classes.py
@@ -1,22 +1,40 @@
-import re
-from datetime import datetime
-from sqlite3 import Date
+from datetime import datetime, date
 
 
 class Post:
-    def __init__(self, name):
+    def __init__(self, name, text):
         self.name = name
+        self.text = text
+        self.post_date = datetime.today()
 
-    def publish_date(self):
-        print(Date.today())
+    def printName(self):
+        print(self.name)
+
+    def printText(self):
+        print(self.text)
 
 
-class Adv(Post):
-    def __init__(self, name, customer):
-        Post.__init__(self, name)
-        self.customer = customer
+class PrivateAd(Post):
+    def __init__(self, name, text, end_date):
+        Post.__init__(self, name, text)
+        self.end_date = datetime.strptime(end_date, '%d/%m/%Y').date()
+        self.delta = (self.end_date - self.post_date.date()).days
 
-    def customer_is(self):
-        print(f'{self.name}: customer is {self.customer}')
+    def printAdv(self):
+        print(f'{self.name} ------------------\n{self.text}\nActual until: {self.end_date}, {self.delta} days left\n------------------------------')
 
-post = Post('News')
+
+class News(Post):
+    def __init__(self, name, text, end_date):
+        Post.__init__(self, name, text)
+        self.end_date = datetime.strptime(end_date, '%d/%m/%Y').date()
+        self.delta = (self.end_date - self.post_date.date()).days
+
+    def printAdv(self):
+        print(f'{self.name} ------------------\n{self.text}\nActual until: {self.end_date}, {self.delta} days left\n------------------------------')
+
+
+post = Post('News', datetime.today())
+
+qqq = PrivateAd('Private Ad', 'TEXT', '31/10/2021')
+qqq.printAdv()
