e60f1dcd700f635cc994b4180743a77fdab14a2d
George Chichenkov <georginus@gmail.com>
2021-10-18 21:46:17 +0300

    1

--- Task06/ImportPost.py
+++ Task06/ImportPost.py
@@ -1,6 +1,6 @@
 import sys
 
-# from Task04_refactor03 import formatText
+from Task04_refactor03 import formatText
 
 sys.path.append('../Task05')
 from Post import Post
@@ -21,26 +21,30 @@ def fileRead(src_file_path='./', src_file_name='task06_src.txt'):
 def parseSrc(src_text):
     for element in src_text:
         parsed_list = element.split('---')
-        #print(parsed_list)
         post_code = parsed_list[0]
         if post_code == '1':
             text = parsed_list[1]
+            src_text_formatted = formatText(text)
+            sentence_formatted = '. '.join(map(lambda s: s.strip().capitalize(), src_text_formatted.split('.')))
             city = parsed_list[2]
-            news = News(text, city)
+            news = News(sentence_formatted, city)
             post = news.printPost()
             print(post)
         elif post_code == '2':
             text = parsed_list[1]
+            src_text_formatted = formatText(text)
+            sentence_formatted = '. '.join(map(lambda s: s.strip().capitalize(), src_text_formatted.split('.')))
             end_date = parsed_list[2]
-            ad = PrivateAd(text, end_date)
+            ad = PrivateAd(sentence_formatted, end_date)
             post = ad.printPost()
             print(post)
-        else:
+        elif post_code == '3':
             text = parsed_list[1]
+            src_text_formatted = formatText(text)
+            sentence_formatted = '. '.join(map(lambda s: s.strip().capitalize(), src_text_formatted.split('.')))
             hashtag = parsed_list[2]
-            lifehack = LifeHack(text, hashtag)
+            lifehack = LifeHack(sentence_formatted, hashtag)
             post = lifehack.printPost()
-            #print(parsed_list[1], ' ', parsed_list[2])
             print(post)
     return parsed_list
 
