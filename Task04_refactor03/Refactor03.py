633f9d1e9af86eb77b7664aba155aa4341b33bc6
George Chichenkov <georginus@gmail.com>
2021-10-18 22:23:40 +0300

    1

--- Task04_refactor03/Refactor03.py
+++ Task04_refactor03/Refactor03.py
@@ -1,10 +1,6 @@
 import re
 
 
-# def source_text(text):
-#     base_text = text
-#     return base_text
-
 def formatText(base_text):
     lower_text = base_text.lower()
     iz_replace_text = lower_text.replace(r"“iz”", r" “iz”").replace(" iz ",
@@ -14,20 +10,23 @@ def formatText(base_text):
 
 
 # Function which returns words list
-def returnWordList(string):
-    str_preformat = '. '.join(map(lambda s: s.strip().capitalize(), string.split('.')))
+def returnWordList(base_text):
+    text_formatted = formatText(base_text)
+    str_preformat = '. '.join(map(lambda s: s.strip().capitalize(), text_formatted.split('.')))
     lis = list(str_preformat.strip().split(" "))  # split by space and converting
     return lis
 
 
 # Function convert list to string
-def listToString(s):
+def listToString(base_text):
+    s = generateLastSentence(base_text)
     str1 = " "
-    return (str1.join(s))
+    return str1.join(s)
 
 
 # last words sentence generating
-def generateLastSentence(list_by_words):
+def generateLastSentence(base_text):
+    list_by_words = returnWordList(base_text)
     for i in list_by_words:
         r = re.compile(r'\b(\w+[.])')
         last_words_list = list(filter(r.match, list_by_words))
@@ -36,14 +35,18 @@ def generateLastSentence(list_by_words):
 
 
 # create Paragraphs list
-def generateParagraphList(text_formatted):
+def generateParagraphList(base_text):
+    text_formatted = formatText(base_text)
     for i in range(len(text_formatted)):
         paragraph_list = list(text_formatted.split("\n\t"))
     return paragraph_list
 
+
 # format Paragraphs and fill temp_text string
-def generateFinalText(paragraph_list, end_with):
+def generateFinalText(base_text, end_with):
     final_text = ''
+    last_sentence = listToString(base_text)
+    paragraph_list = generateParagraphList(base_text)
     for sentence in range(len(paragraph_list)):
         sentence_formatted = '. '.join(map(lambda s: s.strip().capitalize(), paragraph_list[sentence].split('.')))
         if sentence_formatted.endswith(end_with):
@@ -51,27 +54,3 @@ def generateFinalText(paragraph_list, end_with):
         else:
             final_text += sentence_formatted + '\n\t'
     return final_text
-
-# Main
-base_text = r"""homEwork:
-	tHis iz your homeWork, copy these Text to variable.
-
-	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
-
-	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
-
-	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
-text_formatted = formatText(base_text)
-list_by_words = returnWordList(text_formatted)  # text in list by words
-last_sentence = listToString(generateLastSentence(list_by_words))
-paragraph_list = generateParagraphList(text_formatted)
-final_text = generateFinalText(paragraph_list, 'add it to the end of this paragraph. ')
-
-
-# print result
-print(final_text)
-
-# print spaces count
-p = re.compile(r'(\s)')
-spaces_list = p.findall(final_text)
-print("number of spaces: ", len(spaces_list))
--- Task04_refactor03/main.py
+++ Task04_refactor03/main.py
@@ -11,12 +11,8 @@ base_text = r"""homEwork:
 	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
 
 	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
-text_formatted = formatText(base_text)
-list_by_words = returnWordList(text_formatted)  # text in list by words
-last_sentence = listToString(generateLastSentence(list_by_words))
-paragraph_list = generateParagraphList(text_formatted)
-final_text = generateFinalText(paragraph_list, 'add it to the end of this paragraph. ')
 
+final_text = generateFinalText(base_text, 'add it to the end of this paragraph. ')
 
 # print result
 print(final_text)
