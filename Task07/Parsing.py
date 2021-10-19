f81f941719dff599c96c46f1b8a3e0cc7964fe29
George Chichenkov <georginus@gmail.com>
2021-10-19 21:27:07 +0300

    1

--- Task07/Parsing.py
+++ Task07/Parsing.py
@@ -1,11 +1,14 @@
 import sys
 import re
-sys.path.append('../Task04_refactor03')
-sys.path.append('../Task05')
-sys.path.append('../Task06')
 
-from ImportPost import fileRead
-from Refactor03 import returnWordList
+
+# sys.path.append('../Task04_refactor03')
+# sys.path.append('../Task05')
+# sys.path.append('../Task06')
+
+# from ImportPost import fileRead
+# from Refactor03 import returnWordList
+
 
 def fileRead(filepath='../Task06/task06_result.txt'):
     f = open(filepath, 'r')
@@ -13,10 +16,20 @@ def fileRead(filepath='../Task06/task06_result.txt'):
     f.close()
     return src_text
 
-list_of_words = re.findall(r'\w+', fileRead())
-# for element in list_of_words:
-#     re.sub(, element)
 
-my_list = [item for item in list_of_words if item.isalpha()]
+def list_of_words(text=fileRead()):
+    temp_list_of_words = re.findall(r'\w+', text)
+    list_of_words = [item for item in temp_list_of_words if item.isalpha()]
+    return list_of_words
+#print(list_of_words())
 
-print(my_list)
+
+def wordsInLower(list_of_words = list_of_words()):
+    words_lower_list = []
+    for word in list_of_words:
+        words_lower = str(word).lower()
+        words_lower_list.append(words_lower)
+    return words_lower_list
+
+
+print(wordsInLower())
