28f7d77caa677a7677f139297b8b905c7c492f68
George Chichenkov <georginus@gmail.com>
2021-10-21 12:37:53 +0300

    1

--- Task08/my_help.py
+++ Task08/my_help.py
@@ -10,4 +10,7 @@
 # with open('my_json_test1.json', 'w') as myfile:
 #     myfile.write(str(qqq))
 n = 'George'
-print(eval("f'My name is {n}'"))
+#print(eval("f'My name is {n}'"))
+a = eval("[1, 2, 3]")
+print(type(a))
+print(a)
