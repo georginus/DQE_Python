d47e92af1da1c790f973a9323b6e772412dd9fa2
George Chichenkov <georginus@gmail.com>
2021-10-21 12:24:00 +0300

    1

---
+++ Task08/my_help.py
@@ -0,0 +1,13 @@
+### JSON
+
+# dict to json
+
+# qqq = {
+#     "key1": "value1"
+#     , "key2": "value2"
+# }
+#
+# with open('my_json_test1.json', 'w') as myfile:
+#     myfile.write(str(qqq))
+n = 'George'
+print(eval("f'My name is {n}'"))
