120c63680dde3794ffb5b41b1e900eaa89a6e7ed
George Chichenkov <georginus@gmail.com>
2021-10-21 13:00:16 +0300

    1

--- Task08/my_help.py
+++ Task08/my_help.py
@@ -3,14 +3,38 @@
 # dict to json
 
 # qqq = {
-#     "key1": "value1"
-#     , "key2": "value2"
+#      "key1": "value1"
+#      , "key2": "value2"
 # }
+# #
+# # with open('my_json_test1.json', 'w') as myfile:
+# #     myfile.write(str(qqq))
+# n = 'George'
+# #print(eval("f'My name is {n}'"))
+# a = eval("[1, 2, 3]")
 #
-# with open('my_json_test1.json', 'w') as myfile:
-#     myfile.write(str(qqq))
-n = 'George'
-#print(eval("f'My name is {n}'"))
-a = eval("[1, 2, 3]")
-print(type(a))
-print(a)
+# b = eval(str(qqq))
+# print(type(b))
+# print(b)
+
+
+### dump - from dict to json
+import json
+
+qqq = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": True,
+    "key4": None
+    }
+
+
+json.dump(qqq, open('my_dump_to_json.json', 'w'))
+
+
+### load - from json to dict
+qqq_load = json.load(open('my_dump_to_json.json'))
+
+print(type(qqq_load))
+print(qqq_load)
+
