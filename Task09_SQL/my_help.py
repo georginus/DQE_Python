0978e9fe7ef06d069ff5cda47bab66c582632706
George Chichenkov <georginus@gmail.com>
2021-11-10 13:19:23 +0300

    1

--- Task09_SQL/my_help.py
+++ Task09_SQL/my_help.py
@@ -9,6 +9,15 @@ connection = sqlite3.connect('task09.db')
 cursor = connection.cursor()
 
 cursor.execute('create table people (first_name text, last_name text, age real)')
+cursor.execute("insert into people values ('George', 'Chichenkov', '42')")
+
+
+connection.commit()
+
+cursor.execute("select * from people")
+
+result = cursor.fetchall()
+print(result)
 
 cursor.close()
 connection.close()
